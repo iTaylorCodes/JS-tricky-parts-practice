c0c5b8b9f5dacb61a745e30d92dcc698c1d2a0f6
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-11-21 21:16:46 -0800

    Completed Curried Add Exercise

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,10 @@
 function curriedAdd(total) {
-
+  if (!total) return 0;
+  return function accumulate(num) {
+    if (!num) return total;
+    total += num;
+    return accumulate;
+  };
 }
 
 module.exports = { curriedAdd };
