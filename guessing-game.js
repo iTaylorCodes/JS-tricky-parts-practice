7750def4c5f189869c35e855f999f6ff006f2038
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-11-21 19:42:20 -0800

    Completed Guessing Game Exercise

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,19 @@
 function guessingGame() {
-
+  const target = Math.floor(Math.random() * 100);
+  let guessCount = 0;
+  let gameWon = false;
+  return function game(guess) {
+    if (gameWon) return "The game is over, you already won!";
+    guessCount++;
+    if (target > guess) {
+      return `${guess} is too low!`;
+    } else if (target < guess) {
+      return `${guess} is too high!`;
+    } else {
+      gameWon = true;
+      return `You win! You found ${target} in ${guessCount} guesses.`;
+    }
+  };
 }
 
 module.exports = { guessingGame };
